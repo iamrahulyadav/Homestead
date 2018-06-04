90d151ba641543ffa10025172b901dd9676794c3
spauldhaliwal <spauldhaliwal@gmail.com>
2018-06-04 16:22:53 -0400

    Background push notification channel_id implementation

--- app/src/main/java/com/spauldhaliwal/homestead/FirebaseMessagingNotificationService.java
+++ app/src/main/java/com/spauldhaliwal/homestead/FirebaseMessagingNotificationService.java
@@ -8,8 +8,10 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -30,16 +32,16 @@ public class FirebaseMessagingNotificationService extends FirebaseMessagingServi
         super.onMessageReceived(remoteMessage);
         Log.d("msg", "onMessageReceived: " + remoteMessage.getData().toString());
 
-
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        Map notification = remoteMessage.getData();
+        Map notificationData = remoteMessage.getData();
 
-        String notificationType = notification.get("type").toString();
-        String notificationSender = notification.get("sender").toString();
+        String notificationType = notificationData.get("type").toString();
+        String notificationSender = notificationData.get("sender").toString();
+        String notificationBody = notificationData.get("body").toString();
 
         String GROUP_KEY_CHAT = "com.spauldhaliwal.homestead.CHAT_NOTIFICATION";
         String GROUP_KEY_JOBS = "com.spauldhaliwal.homestead.JOB_NOTIFICATION";
@@ -52,7 +54,7 @@ public class FirebaseMessagingNotificationService extends FirebaseMessagingServi
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentTitle(notificationSender + ": ")
-                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setContentText(notificationBody)
                     .setAutoCancel(true)
                     .setContentIntent(pendingIntent)
                     .setDefaults(DEFAULT_SOUND | DEFAULT_VIBRATE)
@@ -71,8 +73,8 @@ public class FirebaseMessagingNotificationService extends FirebaseMessagingServi
         } else if (notificationType.equals(JobsContract.TYPE)) {
 
             channelId = "New Jobs Notification";
-            String jobNotificationTitle = notification.get("jobTitle").toString();
-            String jobNotificationDescription = notification.get("jobDescription").toString();
+            String jobNotificationTitle = notificationData.get("jobTitle").toString();
+            String jobNotificationDescription = notificationData.get("jobDescription").toString();
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
                     .setSmallIcon(R.mipmap.ic_launcher)
